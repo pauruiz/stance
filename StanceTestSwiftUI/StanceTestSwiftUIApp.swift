b13ab35752b0976790904d730eb5cf014d7785f4
Pau <pau@iosnow.net>
2024-08-22 17:40:12 +0100

    Initial Commit

---
+++ StanceTestSwiftUI/StanceTestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanceTestSwiftUIApp.swift
+//  StanceTestSwiftUI
+//
+//  Created by Pau Ruiz Perez on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct StanceTestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
